6755074c17bf8c56ccf875186a08c493d82e14b8
anjanidwilestari <ayianjani774@gmail.com>
2024-07-09 10:15:27 +0700

    Update Migrations

--- database/migrations/2024_07_09_031330_create_surats_table.php
+++ database/migrations/2024_07_09_031330_create_surats_table.php
@@ -9,13 +9,16 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
-        Schema::create('surats', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+    public function up()
+{
+    Schema::create('surats', function (Blueprint $table) {
+        $table->id();
+        $table->string('judul');
+        $table->string('file_path');
+        $table->foreignId('kategori_id')->constrained();
+        $table->timestamps();
+    });
+}
 
     /**
      * Reverse the migrations.
