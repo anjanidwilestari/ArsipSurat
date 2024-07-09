a5603096fdd8150503a6a93677412a3c961c46c9
anjanidwilestari <ayianjani774@gmail.com>
2024-07-09 10:14:10 +0700

    php artisan make:model Surat -mcr

---
+++ resources/views/about.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Halaman About</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+
+    <body class="font-sans antialiased dark:bg-black dark:text-white/50">
+        <h1>Halaman About</h1>
+        <script src="js/script.js"></script>
+    </body>
+</html>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Halaman Home</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body class="font-sans antialiased dark:bg-black dark:text-white/50">
+        <h1>Halaman Home</h1>
+    </body>
+</html>
