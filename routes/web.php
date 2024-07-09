f6224e1610a900e532166e3e16b5c1f688830980
anjanidwilestari <ayianjani774@gmail.com>
2024-07-09 10:31:43 +0700

    Web Routes

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SuratController;
+use App\Http\Controllers\KategoriController;
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::get('/', [SuratController::class, 'index'])->name('surats.index');
+
+Route::resource('surats', SuratController::class);
+
+Route::resource('kategoris', KategoriController::class);
 
 Route::get('/about', function () {
     return view('about');
-});
+})->name('about');
