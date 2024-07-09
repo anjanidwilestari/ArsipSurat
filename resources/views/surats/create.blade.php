218b72daa101c87320effc83f109681860b86301
anjanidwilestari <ayianjani774@gmail.com>
2024-07-09 10:24:42 +0700

    Create View for Surat

---
+++ resources/views/surats/create.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="row mt-5">
+    <div class="col-md-12">
+        <h2>Arsipkan Surat</h2>
+        <form action="{{ route('surats.store') }}" method="POST" enctype="multipart/form-data">
+            @csrf
+            <div class="form-group">
+                <label for="judul">Judul Surat</label>
+                <input type="text" name="judul" class="form-control" required>
+            </div>
+            <div class="form-group">
+                <label for="kategori_id">Kategori</label>
+                <select name="kategori_id" class="form-control" required>
+                    @foreach($kategoris as $kategori)
+                        <option value="{{ $kategori->id }}">{{ $kategori->nama }}</option>
+                    @endforeach
+                </select>
+            </div>
+            <div class="form-group">
+                <label for="file">File Surat (PDF)</label>
+                <input type="file" name="file" class="form-control-file" required>
+            </div>
+            <button type="submit" class="btn btn-success">Simpan</button>
+            <a href="{{ route('surats.index') }}" class="btn btn-secondary">Kembali</a>
+        </form>
+    </div>
+</div>
+@endsection
